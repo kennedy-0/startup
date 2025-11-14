aae991b19ff778378d1ea9fc438d162e15b59b92
Kennedy <danielkennedi121@gmail.com>
2025-11-13 23:05:33 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+  document.getElementById("inicio").onclick = function() {
+    document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+  };
+
+  document.getElementById("projetos").onclick = function() {
+    document.getElementById("projetos-section").scrollIntoView({ behavior: "smooth" });
+  };
+
+  document.getElementById("sobre").onclick = function() {
+    document.getElementById("sobre-section").scrollIntoView({ behavior: "smooth" });
+  };
